0588fa31e2afd6e0726be6045e7430cbccc790d8
joyounjung <younjung8956@naver.com>
2024-04-17 16:39:14 +0900

    02_intropage

--- src/js/intro.js
+++ src/js/intro.js
@@ -1,3 +1,19 @@
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 export function intro(){
-    
+gsap.registerPlugin(ScrollTrigger)
+const frameCount=38
+let offsetValue=960
+
+gsap.to('.img', {
+    backgroundPosition: (-offsetValue * frameCount) + 'px',
+    ease: 'steps(' + (frameCount / 2) + ')',  
+    scrollTrigger: {
+        trigger: '#intro',
+        start: 'top top',
+        end: '+=' + ((frameCount * offsetValue) / 5),  
+        pin: true,
+        scrub: true
+    }
+});
 }
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,3 +1,13 @@
 export function menu(){
-    
+    const headerToggle=document.getElementById('headerToggle')
+    const headerNav=document.querySelector('.header_nav')
+
+    if(headerToggle){
+        headerToggle.addEventListener('click',()=>{
+            headerNav.classList.toggle("show");
+            headerToggle.getAttribute("aria-expanded") === 'true' //조건식
+            ? headerToggle.setAttribute('aria-expanded','false') //참이면
+            : headerToggle.setAttribute('aria-expanded','true') //거짓이면
+        });
+    }
 }
