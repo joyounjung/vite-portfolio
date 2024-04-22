1545892191b939ca5f0410c33457d1a194d312ff
joyounjung <younjung8956@naver.com>
2024-04-22 16:42:11 +0900

    03graphics 완성

--- src/js/graphics.js
+++ src/js/graphics.js
@@ -1,3 +1,26 @@
-export function graphics(){
-    
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+export function graphics() {
+    gsap.utils.toArray('.img_box').forEach(function (imgBox) {
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: imgBox,
+                start: '50% 100%',
+                toggleClass: { 'targets': imgBox, className: 'active' },
+                scrub: 1
+            }
+        })
+        
+    })
+    gsap.utils.toArray('.img_text').forEach(function (imgText) {
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: imgText,
+                start: '50% 100%',
+                toggleClass: { 'targets': imgText, className: 'active' },
+                scrub: 1
+            }
+        })
+        
+    })
 }
--- src/js/intro.js
+++ src/js/intro.js
@@ -3,15 +3,15 @@ import ScrollTrigger from 'gsap/ScrollTrigger';
 export function intro(){
 gsap.registerPlugin(ScrollTrigger)
 const frameCount=38
-let offsetValue=960
+let offsetValue=100
 
 gsap.to('.img', {
-    backgroundPosition: (-offsetValue * frameCount) + 'px',
+    backgroundPosition: (-offsetValue * frameCount) + '%',
     ease: 'steps(' + (frameCount / 2) + ')',  
     scrollTrigger: {
         trigger: '#intro',
         start: 'top top',
-        end: '+=' + ((frameCount * offsetValue) / 5),  
+        end: '+=' + ((frameCount * offsetValue) / 2),  
         pin: true,
         scrub: true
     }
